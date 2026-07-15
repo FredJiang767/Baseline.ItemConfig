d239db040a8413c45c2dc161e97795b3b8096b87
Fred Jiang <Fred.Jiang@aspiraconnect.com>
2026-07-15 22:15:40 +0800

    Add CatalogController

---
+++ Baseline.ItemConfig.API/Controllers/CatalogController.cs
@@ -0,0 +1,28 @@
+﻿using Baseline.ItemConfig.Application;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Baseline.ItemConfig.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]s")]
+    public class CatalogController : ControllerBase
+    {
+        private readonly UiTabService _uiTabService;
+        private readonly ItemService _itemService;
+
+        public CatalogController(UiTabService uiTabService, ItemService itemService)
+        {
+            _uiTabService = uiTabService;
+            _itemService = itemService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> GetItems()
+        {
+            var result = await _uiTabService.GetAll();
+            var items = await _itemService.GetAll();
+
+            return Ok(result);
+        }
+    }
+}
---
+++ Baseline.ItemConfig.Application/DTOs/CatalogDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baseline.ItemConfig.Application.DTOs
+{
+    public class CatalogDto
+    {
+        public List<UiTabReadDto> Tabs
+        {
+            get;
+            set;
+        }
+    }
+}
