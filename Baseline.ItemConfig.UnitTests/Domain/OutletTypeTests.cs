346629ceb1078ce8ae8f598488a955449e49f588
Fred Jiang <Fred.Jiang@aspiraconnect.com>
2026-07-18 19:24:59 +0800

    Add unit test

---
+++ Baseline.ItemConfig.UnitTests/Domain/OutletTypeTests.cs
@@ -0,0 +1,24 @@
+﻿using Baseline.ItemConfig.Domain;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Baseline.ItemConfig.UnitTests.Domain
+{
+    public class OutletTypeTests
+    {
+        [Fact]
+        public void Should_Create_OutletType_Succeed()
+        {
+            // Act
+            var result = OutletType.Create("Internet");
+
+            // Assert
+            Assert.Equal("Internet", result.Name);
+        }
+    }
+}
